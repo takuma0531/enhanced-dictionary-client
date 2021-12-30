675a62212b6d3d7e612ddaf6e7961e111ee4b879
takuma masaki <tb.masaki0531@gmail.com>
2021-12-30 11:07:24 +0200

    add private route

---
+++ src/components/routing/privateRoute/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Route, Redirect, RouteProps } from "react-router-dom";
+
+interface PrivateRouteProps extends RouteProps {
+  isAuthenticated: boolean;
+  redirectTo?: string;
+  isPublic?: boolean;
+}
+
+export default function Privateroute({
+  isAuthenticated,
+  redirectTo,
+  isPublic = false,
+  ...routeProps
+}: PrivateRouteProps) {
+  return isPublic ? (
+    isAuthenticated ? (
+      <Redirect to={`${redirectTo || "/"}`} />
+    ) : (
+      <Route {...routeProps} />
+    )
+  ) : isAuthenticated ? (
+    <Route {...routeProps} />
+  ) : (
+    <Redirect to={`${redirectTo || "/login"}`} />
+  );
+}
